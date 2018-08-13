b4d2162249322803b85486f1068b7a66706500a0
Mohammed Alani <apple@Apples-MacBook-Pro-5.local>
2018-08-13 10:47:01 +0300

    zain cash test api register

---
+++ app/Http/Controllers/APIRgister.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Requsest;
+use App\User;
+use JWTFactory;
+use Validator;
+use Response;
+use JWTAuth;
+
+class APIRgister extends Controller
+{
+    public function register( Request $request){
+        //input information of the user
+        $validator = Validator::make($request -> all(),[
+         'email' => 'required|string|email|max:255|unique:users', //uniqe , email fashion and non empty
+         'name' => 'required', //non empty
+         'age'=> 'required|integer' // vrifed is integer and non empty
+        ]);
+        
+        if($validator -> fails()) {
+           
+            return response()->json($validator->errors());
+            
+        }
+        else{
+            User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'age'=> $request->get('age'),
+            ]);
+            $user = User::first();
+            $token = JWTAuth::fromUser($user);
+            return Response::json(true);
+            }
+
+        
+    }
+}
